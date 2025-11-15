1c83357268d1f4a0aa274f8a989cfe5ae9499ca7
Hriday Samdani <hridaysamdani2330@gmail.com>
2025-11-15 19:56:11 +0530

    Added members in organising committee

--- src/pages/Organising.tsx
+++ src/pages/Organising.tsx
@@ -64,10 +64,10 @@ const Organising = () => {
   ];
 
   const btechStudents: Person[] = [
-    { name: "Mr. Hriday K. Samdani", title: "", initials: "HKS", imageSrc: "/images/dignitaries/hriday.png", description: "AIML Enthusiast" },
-    // { name: "Student 2", title: "", initials: "S2", imageSrc: "" },
-    // { name: "Student 3", title: "", initials: "S3", imageSrc: "" },
-    // { name: "Student 4", title: "", initials: "S4", imageSrc: "" },
+    { name: "Mr. Hriday K. Samdani", title: "", initials: "HKS", imageSrc: "/images/dignitaries/hriday.png", description: "AIML Enthusiast, AI in Drug Discovery and Development" },
+    { name: "Ms. Diya Sharma", title: "", initials: "DS", imageSrc: "/images/diya.png", description: "Antisolvent crystallization and its application in improving the solubility and solid-state properties of pharmaceutical drugs" },
+    { name: "Ms. Vedica Mahendian", title: "", initials: "VM", imageSrc: "/images/Vedica.png", description: "Nanotechnology and applications of engineering in the pharmaceutical sector, sustainable energy and waste valorization." },
+    { name: "Ms. Akshita Jain", title: "", initials: "AJ", imageSrc: "/images/akshita.png" },
   ];
 
   // --- RENDER HELPERS ---
